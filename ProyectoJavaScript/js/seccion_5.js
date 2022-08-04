802f755d9fe8c387a8a56ebaeb4659d7c390646f
brayan0087 <102614416+brayan0087@users.noreply.github.com>
2022-08-04 15:12:45 -0500

    Update  Project

--- ProyectoJavaScript/js/seccion_5.js
+++ ProyectoJavaScript/js/seccion_5.js
@@ -1,7 +1,14 @@
 const d = document,
 w=window;
+// prueba de json 
+var empleado ={
 
+    nombre:'thomas',
+    profesion:'dev'
+};
 
+console.log (JSON.stringify(empleado));
+// termina prueba json
 export function responsiveTester(form){
 const $form = d.getElementById (form);
 let tester;
@@ -10,12 +17,27 @@ let tester;
 d.addEventListener("submit", e=>{ //el evento submit se activa cuando form envia un formulario 
 
     if (e.target === $form) {
-       alert("Formulario Enviado");
-    }
 
-})
+       e.preventDefault(); 
+       //alert("Formulario Enviado");
 
 
+        tester = w.open($form.url.value,"tester", `innerWidth=${$form.Ancho.value},innerHeight=${$form.Alto.value}`);  //se accde al input del form mediante notacion del punto
+
+        
+
+    };
+
+
+});
+
+d.addEventListener("click", e=>{
+
+    if (e.target === $form.cerrar)tester.close();
+    
+    
+    
+});
 
 }
 
